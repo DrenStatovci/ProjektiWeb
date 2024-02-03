b31026b79f70a839f7d0e4b0adf81b9643ef67a7
No1reFS <apexno1re@gmail.com>
2024-02-03 19:10:22 +0100

    News, Categories, Index update

--- admin/index.php
+++ admin/index.php
@@ -1,52 +1,104 @@
-
 <?php
-use Admin\Lib\News;
+include_once 'inc/header.php';
+include_once 'admin/lib/News.php';
 
-include 'inc/header.php';
-include 'inc/sidebar.php';
-include 'lib/News.php';
+$latestNews = new Admin\Lib\News();
+$latestNews = $latestNews->getLatestNews();
 
-if ($_SESSION['role'] == 'user') {
-    header("Location:../index.php");
-}
+$randomNews = new Admin\Lib\News();
+$randomNews = $randomNews->getRandomNews(3);
+
+$lastTech = new Admin\Lib\News();
+$lastTech = $lastTech->getLastByCategory('Technology');
+
+$lastSport = new Admin\Lib\News();
+$lastSport = $lastSport->getLastByCategory('Sport');
+
+$lastHealth = new Admin\Lib\News();
+$lastHealth = $lastHealth->getLastByCategory('Health');
+
+$lastMusic = new Admin\Lib\News();
+$lastMusic = $lastMusic->getLastByCategory('Music');
 ?>
+<div class="body">
+    <div class="main-body">
+        <div class="sport4-template">
+            <a href="single-page.php?nid=<?php echo $latestNews->getId(); ?>"><img class="sport4-img"
+                    style="display: block;" src="images/<?php echo $latestNews->getImage(); ?>"></a>
+            <div class="sport4-text">
+                <a class="sport4-link" href="single-page.html">
+                    Latest News: <br>
+                    <?php echo $latestNews->getTitle(); ?>
+                </a>
+            </div>
+        </div>
 
 
-<div class="dashboardContainer">
-    <h1>News</h1>
-    <div class="tableContainer">
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Image</th>
-                <th>Category</th>
-                <th>Author</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            <?php
-            $allNews = new News();
-            $allNews = $allNews->getAllNews();
-            foreach ($allNews as $news) {
-                echo "  
-                    <tr>
-                        <td>" . $news->getId() . "</td>
-                        <td>" . $news->getTitle() . "</td>
-                        <td>" . $news->getDescription() . "</td>
-                        <td><img src='../images/".$news->getImage()."' width='150px'></td>
-                        <td>" . $news->getCategory() . "</td>
-                        <td>" . $news->getAuthor() . "</td>
-                        <td><a href='editNews.php?nid=" . $news->getId() . "'>Edit</a></td>
-                        <td><a href='deleteNews.php?nid=" . $news->getId() . "'>Delete</a></td>      
-                    </tr>
-                    ";
-            }
-            ?>
-        </table>
-        <a href="createNews.php" class="createUser">Create News</a>
+        <div>
+            <div class="home-categories-big">
+                <?php
+                foreach ($randomNews as $random) {
+                    echo
+                        "<div class='home-categories-big-template'>
+                    <a href='single-page.php?nid= " . $random->getId() . "'>
+                        <img class='categories-big-img' src='images/" . $random->getImage() . "'>
+                        <div class='tech-big-text'>" . $random->getTitle() . "</div>
+                    </a>
+                </div>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="home-categories">
+        <div class="home-categories-template">
+            <a href="single-page.php?nid=<?php echo $lastHealth->getId(); ?>"><img class="categories-img"
+                    src="images/<?php echo $lastHealth->getImage(); ?>">
+                <div class="health-home">
+                    <?php echo $lastHealth->getCategory(); ?>
+                </div>
+                <div class="health-text">
+                    <?php echo $lastHealth->getTitle(); ?>
+                </div>
+            </a>
+        </div>
+
+        <div class="home-categories-template">
+            <a href="single-page.php?nid=<?php echo $lastMusic->getId(); ?>"><img class="categories-img"
+                    src="images/<?php echo $lastMusic->getImage(); ?>">
+                <div class="music-home">
+                    <?php echo $lastMusic->getCategory(); ?>
+                </div>
+                <div class="music-text">
+                    <?php echo $lastMusic->getTitle(); ?>
+                </div>
+            </a>
+        </div>
+
+        <div class="home-categories-template">
+            <a href="single-page.php?nid=<?php echo $lastTech->getId(); ?>"><img class="categories-img"
+                    src="images/<?php echo $lastTech->getImage(); ?>">
+                <div class="tech-home">
+                    <?php echo $lastTech->getCategory(); ?>
+                </div>
+                <div class="tech-text">
+                    <?php echo $lastTech->getTitle(); ?>
+                </div>
+            </a>
+        </div>
+
+        <div class="home-categories-template-sport">
+            <a href="single-page.php?nid=<?php echo $lastSport->getId(); ?>"><img class="categories-img"
+                    src="images/<?php echo $lastSport->getImage(); ?>">
+                <div class="sport-home">
+                    <?php echo $lastSport->getCategory(); ?>
+                </div>
+                <div class="sport-text">
+                    <?php echo $lastSport->getTitle(); ?>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
-</div>
-</body>
+<?php include_once 'inc/footer.php' ?>
